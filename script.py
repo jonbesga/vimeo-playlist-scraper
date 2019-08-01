3a6200670c2d0465634579ecf6708f1f5a536535
Jon Besga <jonan.bsg@gmail.com>
2019-08-01 15:48:54 +0100

    Finish script

--- script.py
+++ script.py
@@ -1,19 +1,14 @@
 import requests
-import re
 import json
 import shutil
 import os
-import html
 import argparse
 import logging
-import zipfile
-
-
-def zipdir(path, ziph):
-    # ziph is zipfile handle
-    for root, dirs, files in os.walk(path):
-        for file in files:
-            ziph.write(os.path.join(root, file))
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support.ui import WebDriverWait as wait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 
 def make_request(url):
@@ -37,43 +32,56 @@ def download_file(url, destination):
 
 
 def create_course_folder(course_title):
+    course_path = f"./downloads/{course_title}"
     try:
-        os.makedirs(f"./{course_title}")
+        os.makedirs(course_path)
     except FileExistsError:
         pass
+    return course_path
 
 
-def get_course_title(data):
-    course_title = re.search("<title>(.*)</title>", data)
-    course_title = course_title.group(1)
-    return html.unescape(course_title)
+def login(driver, login_url, username, password):
+    print("Logging into the domain")
+    driver.get(login_url)
+    driver.find_element_by_id("iump_login_username").send_keys(username)
+    driver.find_element_by_id("iump_login_password").send_keys(password)
+    driver.find_element_by_xpath('//*[@id="ihc_login_form"]/div[6]/input').click()
+    print("Logging successful")
 
 
-def log(text):
-    logging.info(text)
-
-
-def process_playlist(url, job_id=1):
-    logging.basicConfig(
-        level=logging.INFO,
-        # filename=f"{job_id}.log",
-        # filemode="a",
-        format="%(levelname)s - %(message)s",
+def get_app_data_script(driver, url):
+    driver.get(url)
+    wait(driver, 30).until(
+        EC.frame_to_be_available_and_switch_to_it(
+            driver.find_element_by_xpath(
+                "/html/body/div[1]/div[4]/div[2]/div/div/div[3]/div[1]/div/div/div/div/div/iframe"
+            )
+        )
     )
+    wait(driver, 30).until(EC.presence_of_element_located((By.ID, "app-data")))
+    data = driver.find_element_by_id("app-data")
+    response = data.get_attribute("innerHTML")
+    driver.close()
+    return response
 
-    response = make_request(url)
 
-    course_title = get_course_title(response.text)
-    create_course_folder(course_title)
+def process_playlist(url, login_url=None, username=None, password=None):
+    options = Options()
+    options.headless = False
+    driver = webdriver.Firefox(options=options)
+    if login_url and username and password:
+        login(driver, login_url, username, password)
 
-    result = re.search(
-        '<script id="app-data" type="application/json">(.*)</script>', response.text
-    )
-    json_data = result.group(1)
+    logging.basicConfig(level=logging.INFO, format="%(levelname)s - %(message)s")
+
+    json_data = get_app_data_script(driver, url)
+
     json_data = json.loads(json_data)
+    course_title = json_data["playlist_data"]["title"]
+    course_path = create_course_folder(course_title)
 
     if not json_data["clips"]:
-        logging.error('No clips')
+        logging.error("No clips")
         return
 
     for clip in json_data["clips"]:
@@ -90,41 +98,19 @@ def process_playlist(url, job_id=1):
                 max_width = video["width"]
                 url = video["url"]
 
-        download_file(url, os.path.join(f"./{course_title}", f"{video_title}.mp4"))
-        log(f"Downloaded {course_title}: {video_title}")
-
-    zipf = zipfile.ZipFile(f"{course_title}.zip", "w", zipfile.ZIP_DEFLATED)
-    zipdir(f"./{course_title}/", zipf)
-    zipf.close()
-    # Move to static folder
+        download_file(url, os.path.join(course_path, f"{video_title}.mp4"))
+        logging.info(f"Downloaded {course_title}: {video_title}")
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--url",
-        help="vimeo embed playlist. Example: https://vimeo.com/showcase/6106917/embed",
-        required=True,
-    )
+    parser.add_argument("--url", help="Url with embed vimeo playlist", required=True)
+    parser.add_argument("--login-url", help="", required=False)
+    parser.add_argument("--password", help="Password", required=False)
+    parser.add_argument("--username", help="Username", required=False)
     args = parser.parse_args()
-    process_playlist(args.url)
+    process_playlist(args.url, args.login_url, args.username, args.password)
 
 
 if __name__ == "__main__":
     main()
-    # example: "https://vimeo.com/showcase/6106917/embed"
-
-# API extension:
-
-# POST /vimeo-scraper/ {"url": "https://vimeo.com/showcase/6106917/embed" }
-# returns { job_id: 2 }
-# Triggers celery task
-
-# GET /logs/2
-# returns logs
-
-# Chrome extension
-# Click to get the vimeo embed
-# POST
-# GET
-# Final link with all files to download
